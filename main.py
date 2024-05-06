9287f616181732e9eb37b5e624397dc4ec74436e
Nathan3-14 <nathan.watson.172@gmail.com>
2024-05-06 16:51:55 +0100

    Added folder and file types

--- main.py
+++ main.py
@@ -4,29 +4,61 @@ import shutil
 from typing import List
 from rich.console import Console
 from rich.tree import Tree
-import sys
 
 #* For clarifying importance or function of line(s) of code     *#
 #? Giving more information on meaning of parts of data types    ?#
 #! Important information that is needed (such as debug info)    !#
 
 
+def icon(hex: str) -> str:
+    return chr(int(hex, 16))
+
 #TODO Create classes for each file type (kinda) TODO#
 class Folder:
-    def __init__(self) -> None:
-        pass
-        #TODO Do stuff here
+    def __init__(self, explorer: "Explorer", folder_dir: str) -> None:
+        self.explorer = explorer
     
+        self.icon = explorer.file_icons["folder"]
+        self.folder_dir = folder_dir
+        self.folder_name = folder_dir.split("/")[-1]
+    
+    def tree(self, recursion: int=1) -> Tree:
+        to_return = Tree(str(self))
+
+        if recursion <= 0:
+            return to_return
+        recursion -= 1
+
+        for child in os.listdir(self.folder_dir):
+            if os.path.isdir(os.path.normpath(f"{self.folder_dir}/{child}")):
+                child_tree = Folder(self.explorer, os.path.normpath(f"{self.folder_dir}/{child}")).tree(recursion)
+            else:
+                child_tree = File(self.explorer, child, self.folder_dir).tree()
+            to_return.add(child_tree)
+
+        return to_return
+
     def __str__(self) -> str:
-        return "f\" folder icon {name}"
+        return f"{icon(self.icon)} {self.folder_name}"
 
 class File:
-    def __init__(self) -> None:
-        pass
-        #TODO Do stuff here
-        #TODO with extension and name stored seperate
+    def __init__(self, explorer: "Explorer", file_name: str, root_dir: str) -> None:
+        self.icons = explorer.file_icons
+        self.file_path = os.path.normpath(f"{root_dir}/{file_name}")
+        self.file_name = file_name
+        self.file_type = file_name.split(".")[-1]
+
+    def tree(self) -> Tree:
+        to_return = Tree(str(self))
+
+        return to_return
+
     def __str__(self) -> str:
-        return "f\" file icon <from extension> {name}"
+        icon_keys = list(self.icons.keys())
+        icon_key = self.file_type if self.file_type in icon_keys else "default"
+        icon_code = self.icons[icon_key]
+        icon_character = icon(icon_code)
+        return f"{icon_character} {self.file_name}"
 
 GLOBAL_FILE_ICONS = {
     "default": "ea7b",
@@ -87,36 +119,24 @@ class Explorer:
             ]
             file_icon = chr(int(file_icon, 16))
 
-            to_return.append(f"{"#" if os.path.isdir(file_path) else ""}{file_icon} {file_name}")
+            to_return.append(f"{file_icon} {file_name}")
         
         return to_return
 
-    def create_tree(self, dir: str, name: str, expand_layer: int=1) -> Tree:
-        if expand_layer <= 0:
-            return Tree("me!!!!")
-        expand_layer -= 1
-        
-        to_return = Tree(dir)
 
-        for file in self.list_dir(dir):
-            if file.startswith("#"): #? is a folder
-                child = to_return.add(self.create_tree(os.path.normpath(f"{dir}/{file.split(" ")[-1]}"), file))
-            child = to_return.add(file)
-
-        return to_return
-
-    def loop(self, expand_layer: int=0) -> None:
+    def loop(self, recursion: int=1) -> None:
         if not self.initialised:
             self.error("Explorer is not initialised", end_program=True)
             return
         
-        file_tree = self.create_tree(os.path.normpath(f"{os.getcwd()}/{self.current_directory}"), f"{self.current_directory}", 4)
+        file_tree = Folder(self, self.current_directory).tree(recursion)
         self.console.print(file_tree)
 
 def test():
     explorer = Explorer(default_dir="./test-files", extra_icons=["./icons.json"])
 
     explorer.loop()
+    explorer.loop(recursion=99)
 
 if __name__ == "__main__":
     test()
